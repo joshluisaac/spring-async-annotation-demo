6e5ffcd666500a71eaa92ced75236ded327dbbf9
Josh <josh@joshuzo.com>
2022-01-15 22:23:11 +1100

    chore: formatting

--- src/main/java/com/joshuzo/asyncannotation/AppConfig.java
+++ src/main/java/com/joshuzo/asyncannotation/AppConfig.java
@@ -1,36 +1,33 @@
 package com.joshuzo.asyncannotation;
 
+import java.util.concurrent.Executor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import java.util.concurrent.Executor;
-
 @Configuration
 @EnableAsync
 public class AppConfig {
 
-    @Bean
-    BatchService batchService(){
-        return new BatchService();
-    }
+  @Bean
+  BatchService batchService() {
+    return new BatchService();
+  }
 
+  @Bean(name = "defaultThreadPoolTaskExecutor")
+  public Executor threadPoolTaskExecutor() {
+    return new ThreadPoolTaskExecutor();
+  }
 
-    @Bean(name = "defaultThreadPoolTaskExecutor")
-    public Executor threadPoolTaskExecutor() {
-        return new ThreadPoolTaskExecutor();
-    }
-
-    @Bean(name = "threadPoolTaskExecutor")
-    public Executor threadPoolTaskExecutor2() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(20);
-        executor.setMaxPoolSize(1000);
-        executor.setWaitForTasksToCompleteOnShutdown(true);
-        executor.setAwaitTerminationSeconds(15);
-        executor.setThreadNamePrefix("Async-");
-        return executor;
-    }
-
+  @Bean(name = "threadPoolTaskExecutor")
+  public Executor threadPoolTaskExecutor2() {
+    ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+    executor.setCorePoolSize(20);
+    executor.setMaxPoolSize(1000);
+    executor.setWaitForTasksToCompleteOnShutdown(true);
+    executor.setAwaitTerminationSeconds(15);
+    executor.setThreadNamePrefix("Async-");
+    return executor;
+  }
 }
--- src/main/java/com/joshuzo/asyncannotation/Application.java
+++ src/main/java/com/joshuzo/asyncannotation/Application.java
@@ -1,29 +1,21 @@
 package com.joshuzo.asyncannotation;
 
+import java.util.stream.IntStream;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.util.stream.IntStream;
-
-
 public class Application {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        ApplicationContext  context = new AnnotationConfigApplicationContext(AppConfig.class);
-        BatchService batchService = context.getBean(BatchService.class);
-        IntStream.range(0,200).forEach(x -> batchService.longRunningMethod());
-        batchService.quickMethod();
-        batchService.quickMethod();
-        batchService.quickMethod();
-        batchService.quickMethod();
-        batchService.quickMethod();
-        System.out.println("Completed from " + Utils.threadName());
-//        Utils.waitFor(6000);
-
-
-    }
-
-
-
+    ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+    BatchService batchService = context.getBean(BatchService.class);
+    IntStream.range(0, 200).forEach(x -> batchService.longRunningMethod());
+    batchService.quickMethod();
+    batchService.quickMethod();
+    batchService.quickMethod();
+    batchService.quickMethod();
+    batchService.quickMethod();
+    System.out.println("Completed from " + Utils.threadName());
+  }
 }
